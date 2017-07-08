f70b27e16f10e2b24d31ab4df6472540bb0ecad3
GreenLantern101 <arctangent20@yahoo.com>
2017-07-08 10:53:46 -0500

    Remove "etag"
    "etag" is Youtube API internal tracking. Update humanities data set.

--- server/khanquality.js
+++ server/khanquality.js
@@ -91,21 +91,22 @@ const KhanQuality = (logger) => {
     options.uri = fullurl;
     return rp(options)
       .then((rawdata) => {
-        // log.info(obj);
         if (!rawdata || rawdata.length == '') { return Promise.reject('Could not get data from url.'); }
-
-        // remove unnecessary data to reduce file size & process memory usage
-        // approx. 75% reduction (tested: 280kb -> 67kb)
-        if (!rawdata.items[0]) {
+        /* code specific to extract info from Youtube video listing API */
+        const data = rawdata.items[0];
+        if (!data) {
           // indicates the video is no longer available for viewing
           log.info(`No video data available for ${obj.title}`);
-          return rawdata;
+          return data;
         }
-        const snip = rawdata.items[0].snippet;
-        snip.description = 'omitted';
-        snip.thumbnails = 'omitted';
-        snip.localized.description = 'omitted';
-        return rawdata;
+        // remove unnecessary data to reduce file size & process memory usage
+        // approx. 80% reduction (tested: 280kb -> 57kb)
+        data.etag = 'omit';
+        const snip = data.snippet;
+        snip.description = 'omit';
+        snip.thumbnails = 'omit';
+        snip.localized.description = 'omit';
+        return data;
       })
       .catch((err) => {
         log.error(err);
