d60d269e5ac861ff8c69890d3ca908085874099a
GreenLantern101 <arctangent20@yahoo.com>
2017-07-09 10:46:31 -0500

    Major refactor dataCollector

--- server/dataCollector.js
+++ server/dataCollector.js
@@ -1,16 +1,91 @@
-'use strict'
+'use strict';
 
-/* Collects data from Khan Academy and Youtube APIs 
-   Intended to be run once.
+/* Collects data from Khan Academy and Youtube APIs.
+   Intended to be run once, with data being stored persistently
 */
 
 // custom logger --> share one instance across all modules/files
 const log = require('./logger.js');
+const Promise = require('bluebird');
 
-const KhanQuality = require('./khanquality.js')(log);
+// TODO: use MongoDB instead of storing as JSON file manually
+
+const kq = require('./khanquality.js')(log);
+
+// promisify IO functions, replacing callbacks
+const writeFile = Promise.promisify(require('fs').writeFile);
+const readFile = Promise.promisify(require('fs').readFile);
+const stat = Promise.promisify(require('fs').stat);
+
+const topicsFilePath = './data/topics.json';
+
+const kaTopicTree = require('./ka-topic-tree.js')(log, false);
+
+/* basic caching */
+// fs.stat returns result if file exists, ENOENT error if file doesn't exist
+const getTopics = rootTopic => stat(topicsFilePath)
+  // if file exists, read from file
+  .then(result => readFile(topicsFilePath).then((contents) => {
+    log.info('Loaded topics from file.');
+    return JSON.parse(contents);
+  }))
+  .catch(err =>
+    // if file doesn't exist (assume not corrupted), then get from API
+    kaTopicTree.getFromAPI(rootTopic)
+    .then((results) => {
+      writeFile(topicsFilePath, JSON.stringify(results)); // write to file
+      log.info('Written topics to file.');
+      return results;
+    }),
+  );
+
+// 1. get topics
+const execute = rootTopic =>
+  getTopics(rootTopic)
+  .tap((topics) => {
+    log.info(`${topics.length} topics.`);
+  })
+  .map(obj =>
+    // 2. get Youtube video ID of each video if needed (is slowest step)
+    kq.getYoutubeID(obj)
+    .then((yid) => {
+      obj.youtubeid = yid;
+      return obj;
+    })
+    // 20 concurrent max to prevent ECONNRESET and ETIMEDOUT
+    , {
+      concurrency: 20,
+    })
+  .tap(obj => log.info('Youtube IDs acquired.'))
+  .then((results) => {
+    writeFile(topicsFilePath, JSON.stringify(results)); // write to file
+    log.info('Written Youtube IDs to file.');
+    return results;
+  })
+  .map(obj =>
+    // 3. get Youtube video info of each video if needed
+    kq.getVideoInfo(obj)
+    .then((info) => {
+      obj.videoInfo = info;
+      return obj;
+    }),
+    // 20 concurrent max to prevent ECONNRESET and ETIMEDOUT
+    {
+      concurrency: 20,
+    })
+  .then((results) => {
+    writeFile(topicsFilePath, JSON.stringify(results)); // write to file
+    log.info('Written Youtube video data to file.');
+    log.info('Done.');
+  })
+  .catch((err) => {
+    log.error(err);
+  });
+
 // the entire topic tree is 30mb :(
 // start off with a root (proof of concept)
-KhanQuality.execute('cells');
+execute('cells');
+
 // global-art-architecture: 19 videos
 // cells: 61 videos
 // humanities: 2465 videos
--- server/khanquality.js
+++ server/khanquality.js
@@ -1,10 +1,8 @@
 /*
-
-Main application-specific module.
+Helper module to retrieve data from APIs
 
 Usage:
 const kq = require('./khanquality.js')(loggerInstance);
-kq.execute();
 
 */
 
@@ -12,57 +10,26 @@ kq.execute();
 
 const KhanQuality = (logger) => {
   const rp = require('request-promise');
-  const assert = require('assert'); // assert funcs don't work w/ promises, which never throw...
+  // NTOE: assert funcs don't work w/ promises, which never throw...
   const Promise = require('bluebird');
   const log = logger;
 
   // TODO: eventually convert to streams to reduce memory usage
 
-  // promisify IO functions, replacing callbacks
-  const writeFile = Promise.promisify(require('fs').writeFile);
-  const readFile = Promise.promisify(require('fs').readFile);
-  const stat = Promise.promisify(require('fs').stat);
-
-  const topicsFilePath = './data/topics.json';
-
   const options = {
     json: true, // Automatically parses the JSON string in the response
   };
 
-  const kaTopicTree = require('./ka-topic-tree.js')(log, false);
-
-  /* basic caching */
-  // fs.stat returns result if file exists, ENOENT error if file doesn't exist
-  const getTopics = rootTopic => stat(topicsFilePath)
-    // if file exists, read from file
-    .then(result => readFile(topicsFilePath).then((contents) => {
-      log.info('Loaded topics from file.');
-      return JSON.parse(contents);
-    }))
-    .catch(err =>
-      // if file doesn't exist (assume not corrupted), then get from API
-      kaTopicTree.getFromAPI(rootTopic)
-      // convert each topic to an obj
-      .map(topicTitle => ({
-        title: topicTitle,
-      }))
-      .then((results) => {
-        // aggregated results, in the form of jagged(nested) array
-        // log.info(results);
-        writeFile(topicsFilePath, JSON.stringify(results)); // write to file
-        log.info('Written topics to file.');
-        return results;
-      }),
-    );
-
   // get corresponding Youtube video ID of a topic - Khan Academy API
   const getYoutubeID = (obj) => {
     // check topic is valid string
-    if (obj === null || obj === undefined) {
+    if (!obj) {
       return Promise.reject('Improper topic');
     }
     // shortcut if already have youtube id
-    if (obj.youtubeid) { return Promise.resolve(obj.youtubeid); }
+    if (obj.youtubeid) {
+      return Promise.resolve(obj.youtubeid);
+    }
 
     const title = obj.title;
     options.uri = `http://www.khanacademy.org/api/v1/videos/${title}`;
@@ -74,24 +41,29 @@ const KhanQuality = (logger) => {
   };
 
   // get Youtube video detailed info from Youtube API
+  // Documentation: https://developers.google.com/youtube/v3/docs/videos
   const getVideoInfo = (obj) => {
     // check youtube video id valid
     const yid = obj.youtubeid;
-    if (yid === null || yid === undefined || typeof yid !== 'string') {
+    if (!yid || typeof yid !== 'string') {
       return Promise.reject('Youtube ID does not exist.');
     }
     // shortcut if already have video info
-    if (obj.videoInfo) { return Promise.resolve(obj.videoInfo); }
+    if (obj.videoInfo) {
+      return Promise.resolve(obj.videoInfo);
+    }
 
     const baseurl = 'https://www.googleapis.com/youtube/v3/videos';
     const key = process.env.kq_youtube_key;
     // note: can't access 'fileDetails' param unless owner of video
     const reqparts = ['snippet', 'contentDetails', 'statistics'].join('%2C');
-    const fullurl = `${baseurl}?id=${yid}` + `&part=${reqparts}` + `&key=${key}`;
+    const fullurl = `${baseurl}?id=${yid}&part=${reqparts}&key=${key}`;
     options.uri = fullurl;
     return rp(options)
       .then((rawdata) => {
-        if (!rawdata || rawdata.length == '') { return Promise.reject('Could not get data from url.'); }
+        if (!rawdata || rawdata.length === 0) {
+          return Promise.reject('Could not get data from url.');
+        }
         /* code specific to extract info from Youtube video listing API */
         const data = rawdata.items[0];
         if (!data) {
@@ -113,50 +85,9 @@ const KhanQuality = (logger) => {
       });
   };
 
-  // main runner
-  // 1. get topics
-  const execute = (rootTopic) => {
-    getTopics(rootTopic).tap((topics) => {
-      log.info(`${topics.length} topics.`);
-    }).map(obj =>
-      // 2. get Youtube video ID of each video if needed (is slowest step)
-      getYoutubeID(obj).then((yid) => {
-        obj.youtubeid = yid;
-        return obj;
-      })
-      // 20 concurrent max to prevent ECONNRESET and ETIMEDOUT
-      , {
-        concurrency: 20,
-      },
-    ).tap(obj => log.info('Youtube IDs acquired.')).then((results) => {
-      writeFile(topicsFilePath, JSON.stringify(results)); // write to file
-      log.info('Written Youtube IDs to file.');
-      return results;
-    }).map(obj =>
-      // 3. get Youtube video info of each video if needed
-      getVideoInfo(obj).then((info) => {
-        obj.videoInfo = info;
-        return obj;
-      }),
-      // 20 concurrent max to prevent ECONNRESET and ETIMEDOUT
-      {
-        concurrency: 20,
-      },
-    ).then((results) => {
-      writeFile(topicsFilePath, JSON.stringify(results)); // write to file
-      log.info('Written Youtube video data to file.');
-      log.info('Done.');
-    }).catch((err) => {
-      log.error(err);
-    });
-  };
-
-  // then use https://developers.google.com/youtube/v3/docs/videos to get data like upvotes/downvotes/comments/etc.
-  // need snippet, contentDetails, statistics
-  // date created?, date updated?
-
   return {
-    execute,
+    getYoutubeID,
+    getVideoInfo,
   };
 };
 module.exports = KhanQuality;
