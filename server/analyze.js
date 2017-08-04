7ac38c82a50af33ab3b2af02d3b6bc49f6f99bc8
cktang88 <arctangent20@yahoo.com>
2017-08-03 21:50:32 -0500

    add querying for viewcount
    Still buggy b/c $where query operator doesn't exist for native Mongo
    driver with node.js

--- server/analyze.js
+++ server/analyze.js
@@ -3,9 +3,30 @@ const log = require('./logger.js');
 const Promise = require('bluebird');
 const db = require('./dbManager.js')(log);
 
+// maybe try mapreduce? https://docs.mongodb.com/manual/reference/command/mapReduce/#dbcmd.mapReduce
+
 db.connect()
   .then(() => {
+    let query = {
+      $where: function(){ // $where is not available for node.js mongo...
+        log.info(this.videoInfo.statistics.viewCount);
+        return false;
+        return Number(this.videoInfo.statistics.viewCount) > 1000000;
+      }
+    };
+    // look into sorting - http://mongodb.github.io/node-mongodb-native/markdown-docs/queries.html#sorting
+    let arr = db.find(query);
+    log.info(arr);
+
+    arr.each((err, doc) => {
+      if (doc)
+        log.info(doc.title);
+      else
+        log.info(doc);
+    });
+
     log.info('hello');
+    return arr;
   })
   .then(db.close) // if omitted, process never ends. If done, then exits with null error.
   .catch(err => log.error(err));
